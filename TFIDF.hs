0891cd37b8ebeaa8f073cbd31ac6b3546a48cdcc
Ben Gamari <bgamari.foss@gmail.com>
2012-10-31 11:12:38 -0400

    Use HashMap for document counts

--- TFIDF.hs
+++ TFIDF.hs
@@ -19,6 +19,7 @@ import           Data.HashSet (HashSet)
 import qualified Data.HashSet as S
 import           Data.Hashable
 import           Data.List
+import           Data.Monoid
 import qualified Data.Text as T
 import Data.String (IsString)       
 
@@ -31,7 +32,7 @@ newtype Term = Term T.Text
              deriving (Show, Eq, Hashable, IsString)
              
 data Corpus = Corpus { _cDocuments :: HashSet Document
-                     , _cTerms :: HashMap Term [Document]
+                     , _cTerms :: HashMap Term (HashMap Document Int)
                      }
 makeLenses ''Corpus
      
@@ -39,38 +40,29 @@ emptyCorpus :: Corpus
 emptyCorpus = Corpus S.empty M.empty
             
 addDocument :: Document -> [Term] -> Corpus -> Corpus
-addDocument d ts c =
-    cDocuments %~ S.insert d
-    $ foldl' (\c t->cTerms %~ M.insertWith (++) t [d] $ c) c ts
-    
-{--            
-addDocument :: Document -> [Term] -> Corpus -> Corpus
 addDocument d ts c = addDocument' d ts' c
-    where 
-    cDocuments %~ S.insert d
-    $ foldl' (\c t->cTerms %~ M.insertWith (++) t [d] $ c) c ts
+    where ts' = foldl' (flip $ \t->M.insertWith (+) t 1) M.empty ts
 
-addDocument' :: Document -> [(Term, Int)] -> Corpus -> Corpus
+addDocument' :: Document -> HashMap Term Int -> Corpus -> Corpus
 addDocument' d ts c =
     cDocuments %~ S.insert d
-    $ foldl' (\c (term,n)->cTerms %~ M.insertWith (++) t [(d,n)] $ c) c ts
---}
+    $ M.foldlWithKey' (\c term n->cTerms %~ M.insertWith (<>) term (M.singleton d n) $ c) c ts
     
 removeDocument :: Document -> Corpus -> Corpus
 removeDocument d =
     (cDocuments %~ S.delete d)
-    . (cTerms %~ M.map (filter (/= d)))
+    . (cTerms %~ M.map (M.delete d))
 
 idf :: Corpus -> Term -> Double
 idf c t = log $ docs / docsWithTerm
     where docs = realToFrac $ views cDocuments S.size c
-          docsWithTerm = realToFrac $ length $ nub $ M.lookupDefault [] t (c^.cTerms)
+          docsWithTerm = realToFrac $ M.size $ M.lookupDefault M.empty t (c^.cTerms)
     
 data Scaling = BoolScaling | LogScaling | LinearScaling | NormedScaling
 
 tf :: Scaling -> Corpus -> Term -> Document -> Score
 tf LinearScaling c t d = 
-    realToFrac $ length $ filter (==d) $ M.lookupDefault [] t (c^.cTerms)
+    realToFrac $ M.lookupDefault 0 d $ M.lookupDefault M.empty t (c^.cTerms)
 tf LogScaling c t d = log $ tf LinearScaling c t d
 tf BoolScaling c t d = if tf LinearScaling c t d > 0 then 1 else 0
 
